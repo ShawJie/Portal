465b9734c8fa3848738d346e92e83dfd17430f64
ShawJie <jieshaofan@qipeng.com>
2023-11-23 16:55:03 +0800

    modify pino placeholder

--- src/router/handlers/AccessControlHandler.js
+++ src/router/handlers/AccessControlHandler.js
@@ -12,7 +12,7 @@ class AccessControlHandler extends RequestHandler {
         }
 
         if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
-            this.#logger.warn("Missing Authorization Header", req.headers);
+            this.#logger.warn("Missing Authorization Header, %o", req.headers);
             return res.status(401).json({ message: 'Missing Authorization Header' });
         }
     
@@ -20,7 +20,7 @@ class AccessControlHandler extends RequestHandler {
         const base64Credentials =  req.headers.authorization.split(' ')[1];
         const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
         if (!app.inAccessSet(credentials)) {
-            this.#logger.warn("Access Denied", credentials);
+            this.#logger.warn("Access Denied, %s", credentials);
             return res.status(403).json({ message: 'Access Denied' });
         }
 
